922ec76243973974c46f0306f1be193dc349e925
Kobus Booyens <kobusb@runninghill.co.za>
2023-06-12 20:10:57 +0200

    Refactor: Extract a  query object.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import {Genre} from "./Hooks/useGenre";
 import {PlatformSelector} from "./components/PlatformSelector";
 import {Platform} from "./Hooks/useGames";
 
+export interface GameQuery {
+    genre: Genre | null,
+    platform: Platform | null
+}
+
 function App() {
     let setTitle = false
     useEffect(() => {
@@ -17,30 +22,28 @@ function App() {
         }
     })
 
-    const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
-    const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
+    const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
 
   return <Grid templateAreas={{
-      base: ` "nav" "main"`,
-      lg: `"nav nav" "aside main"`
-  }}
-  templateColumns={{
-      base: 'fr1',
-      lg: '200px 1fr'
-  }}>
-      <GridItem area={'nav'}><NavBar></NavBar></GridItem>
-      <Show above="lg">
-          <GridItem area="aside" paddingX='5'>
-              <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} selectedGenre={selectedGenre}></GenreList>
-          </GridItem>
-      </Show>
+              base: ` "nav" "main"`,
+              lg: `"nav nav" "aside main"`
+              }}
+              templateColumns={{
+                  base: 'fr1',
+                  lg: '200px 1fr'
+              }}>
+              <GridItem area={'nav'}><NavBar></NavBar></GridItem>
+              <Show above="lg">
+                  <GridItem area="aside" paddingX='5'>
+                      <GenreList onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})} selectedGenre={gameQuery.genre}></GenreList>
+                  </GridItem>
+              </Show>
 
-      <GridItem area={'main'}>
-          <PlatformSelector onSelectedPlatform={(platform) => setSelectedPlatform(platform)} selectedPlatform={selectedPlatform}></PlatformSelector>
-          <GameGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform} ></GameGrid>
-      </GridItem>
-
-  </Grid>
+              <GridItem area={'main'}>
+                  <PlatformSelector onSelectedPlatform={(platform) => setGameQuery({...gameQuery, platform})} selectedPlatform={gameQuery.platform}></PlatformSelector>
+                  <GameGrid gameQuery={gameQuery} ></GameGrid>
+              </GridItem>
+          </Grid>
 }
 
 export default App
