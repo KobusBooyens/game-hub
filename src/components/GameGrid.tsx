6da7309251b05682c5cea8ba102ef9b08bbcc09e
Kobus Booyens <kobusb@runninghill.co.za>
2023-06-13 19:41:17 +0200

    Refactored game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,9 +13,10 @@ interface Props{
 export const GameGrid = ({gameQuery}: Props) => {
     const {data, error, isLoading} = useGames(gameQuery)
     const skeletons = Array.from({ length: 12 }, (value, index) => index)
+
+    if(error) return <Text>{error}</Text>
+
     return (
-        <>
-            {error && <Text>{error}</Text>}
             <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={6}>
                 {isLoading && skeletons.map(skeleton =>
                     <GameCardContainer key={skeleton}>
@@ -29,6 +30,5 @@ export const GameGrid = ({gameQuery}: Props) => {
                     </GameCardContainer>
                 )}
             </SimpleGrid>
-        </>
     )
 }
