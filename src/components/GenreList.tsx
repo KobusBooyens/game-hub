206bc4d4a55184b3668871cf77f173cc3e04ccb8
Kobus Booyens <kobusb@runninghill.co.za>
2023-06-16 15:41:23 +0200

    Implemented React Query for Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -23,7 +23,7 @@ export const GenreList = ({onSelectGenre, selectedGenre} : Props) => {
         <>
             <Heading fontSize={"2xl"} marginBottom={3}>Genres</Heading>
             <List >
-                {data.map(g =>
+                {data?.results.map(g =>
                     <ListItem key={g.id} paddingY='5px'>
                         <HStack>
                             <Image boxSize="32px"
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import theme from "./theme";
 import './index.css'
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
 
 const queryClient = new QueryClient()
 
